b816849d4ecd1527b512dc44d1116523324bfb52
CHOI_INSEONG <cINSEONG0625@gmail.com>
2023-04-29 21:05:09 +0900

    Inorder + Preorder + Postorder + Levelorder

--- Data.c
+++ Data.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#define MAX_QUEUE 10
 typedef int BData;
 
 // Binary트리를 만들기 위한 구조체 선언
@@ -8,6 +8,52 @@ typedef struct _bTreeNode{
     struct _bTreeNode* left_child;
     struct _bTreenode* right_child;
 }BTreeNode;
+
+typedef struct {
+    int rear;
+    int front;
+    Data items[MAX_QUEUE];
+}Queue;
+
+void InitQueue(Queue* queue) {
+    queue->front = queue->rear = 0;
+}
+
+bool IsFull(Queue* queue) {
+    return queue->front == (queue->rear + 1) % MAX_QUEUE;
+}
+
+bool IsEmpty(Queue* queue) {
+    return queue->front == queue->rear;
+}
+
+Data peek(Queue* queue) {
+    if (IsEmpty(queue)) {
+        exit(1);
+    }
+    else {
+        return queue->items[queue->front];
+    }
+}
+
+void EnQueue(Queue* queue, Data item){
+    if (IsFull(queue)) {
+        exit(1);
+    }
+    else {
+        queue->items[queue->rear] = item;
+        queue->rear = (queue->rear + 1) % MAX_QUEUE;
+    }
+}
+
+void DeQueue(Queue* queue) {
+    if (IsEmpty(queue)) {
+        exit(1);
+    }
+    else {
+        queue->front = (queue->front + 1) % MAX_QUEUE;
+    }
+}
 //Inorder방식(LCR)
 void Inorder(BTreeNode* root){
     if (root != NULL){ 
@@ -34,6 +80,25 @@ void Postorder(BTreeNode* root){
         printf("%d ", root->item); // (C)
     }
 }
+
+void LevelOrder(BTreeNode* root){
+    Queue queue;
+    if(root == NULL) return;
+    
+    InitQueue(&queue);
+    EnQueue(&Queue, root);
+    while(!IsEmpty(&queue)){
+        root = Peek(&queue);
+        DeQueue(&queue);
+
+        printf("%d ", root->item);
+        if(root->left_child != NULL) Enqueue(&queue, root->left_child);
+        if(root->right_child != NULL) EnQueue(&queue, root->right_child);
+    }
+}
+
+
+
 int main(){
 
 }
