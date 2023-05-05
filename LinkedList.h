8893613e35d19d3438dfa9fe8f7ff868b7287a56
CHOI_INSEONG <cINSEONG0625@gmail.com>
2023-05-05 21:38:31 +0900

    LinkedList

---
+++ LinkedList.h
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int Data;
+
+typedef struct _Node{
+    Data item;
+    struct _Node* next;
+}Node;
+
+typedef struct{
+    Node* head;
+    int len;
+}LinkedList;
+
+void InitList(LinkedList* plist){
+    plist->head = (Node*)malloc(sizeof(Node));
+    plist->head->next = NULL;
+    plist->len = 0;
+}
+
+bool IsEmpty(LinkedList* plist){
+    return plist->len == 0;
+}
+
+void InsertMiddle(LinkedList* plist,int pos, Data item){
+    Node* cur = plist->head->next;
+    Node* newnode = (Node*)malloc(sizeof(Node));
+    for(int i = 0; i < pos; i++){
+        cur = cur->next;
+    }
+    newnode->item = item;
+    newnode->next = cur->next;
+    cur->next = newnode;
+    plist->len++;
+}
+
+void RemoveItem(LinkedList* plist, int pos){
+    Node* cur, *temp;
+    if(IsEmpty(plist) || pos < 0 || pos >= plist->len){
+        exit(1);
+    }
+    for(int i = 0;i < pos; i++){
+        cur = cur->next;
+    }
+    temp = cur->next;
+    cur->next = temp->next;
+    plist->len--;
+    free(temp);
+}
+
+Data ReadItem(LinkedList* plist, int pos){
+    Node* cur = plist->head->next;
+    for(int i = 0; i < pos; i++){
+        cur = cur->next;
+    }
+    return cur->item;
+}
+
+
