d5f074ec05ecfb9f4ff1f758b73c549a390c0e83
ChoiInSeong <cINSEONG0625@gmail.com>
2023-06-09 08:50:46 +0900

    Sort

--- StructureHeader/Sort.h
+++ StructureHeader/Sort.h
@@ -15,3 +15,30 @@ void SelectionSort(Data* list, int n){
         SWAP(list[i], list[min], temp);
     }
 }
+
+void BubbleSort(Data* list, int n){
+    int temp;
+    for (int i = n-1; i > 0; i--){
+        for (int j = 0; j < i; j++){
+            if(lsit[j] > list[j+1]){
+                SWAP(list[j], list[j + 1], temp);
+            }
+        }
+    }
+}
+
+void InsertionSort(Data* list, int n){
+    int j, key;
+    for (int i = 1; i < n; i++){
+        key = list[i];
+        for (j = i - 1; j >= 0; j--){
+            if(key < list[j]){
+                list[j + 1] = list[j];
+            }
+            else{
+                break;
+            }
+        }
+        list[j + 1] = key;
+    }
+}
