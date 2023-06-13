6235d158998655dcb749abe04dc60d9fbc452640
ChoiInSeong <cINSEONG0625@gmail.com>
2023-06-13 09:32:09 +0900

    Assignment3

--- MineSweeper.c
+++ MineSweeper.c
@@ -5,23 +5,40 @@ int grid[NUM][NUM];
 // MODIFY THIS CODE
 int get_mine(int m, int n) {
 	int result = 0;
+    bool visited[m][n] ={false, };
+
+
+
+
+
 	return result;
 }
 
 
 
 int main() {
-	/* DO NOT MODIFY */
-	int i=0, j=0, m=0, n=0, result=0;
+	// /* DO NOT MODIFY */
+	// int i=0, j=0, m=0, n=0, result=0;
 
-	scanf("%d %d", &m, &n);
-	for(i=0; i<m; i++) {
-		for (j=0; j<n; j++) {
-			scanf("%d", &grid[i][j]);
-		}
-	}
+	// scanf("%d %d", &m, &n);
+	// for(i=0; i<m; i++) {
+	// 	for (j=0; j<n; j++) {
+	// 		scanf("%d", &grid[i][j]);
+	// 	}
+	// }
 	
-	result=get_mine(m,n);
-	printf("%d\n", result);
-	/* DO NOT MODIFY */
+	// result=get_mine(m,n);
+	// printf("%d\n", result);
+	// /* DO NOT MODIFY */
+    bool visited[4][5] = {false, };
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 5; j++){
+            printf("%d", visited[4][5]);
+        }
+    }
+
+
+
+
+
 }
